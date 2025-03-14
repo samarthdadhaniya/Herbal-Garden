a9b83131b2309e3101f0d3cb806441609b707fd9
Samarth Dadhaniya <samarthdadhaniya7@gmail.com>
2025-03-15 00:39:54 +0530

    :hammer: update App.tsx file also add admin routes

--- src/App.tsx
+++ src/App.tsx
@@ -14,30 +14,54 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
 import Contact from "./pages/Contact";
+import Profile from "./pages/Profile";
+import { AuthProvider } from "./contexts/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
+import AdminRoute from "./components/AdminRoute";
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/plants" element={<PlantsDatabase />} />
-          <Route path="/plants/:id" element={<PlantDetails />} />
-          <Route path="/garden" element={<Garden />} />
-          <Route path="/tours" element={<VirtualTours />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <AuthProvider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/home" element={<Index />} />
+            <Route path="/plants" element={<PlantsDatabase />} />
+            <Route path="/plants/:id" element={
+              <ProtectedRoute>
+                <PlantDetails />
+              </ProtectedRoute>
+            } />
+            <Route path="/garden" element={
+              <ProtectedRoute>
+                <Garden />
+              </ProtectedRoute>
+            } />
+            <Route path="/tours" element={<VirtualTours />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/admin" element={
+              <AdminRoute>
+                <Admin />
+              </AdminRoute>
+            } />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/profile" element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            } />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </TooltipProvider>
+    </AuthProvider>
   </QueryClientProvider>
 );
 
